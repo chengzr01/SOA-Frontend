df8bb1ce2415e23ff74009d582d5219d3692a8f0
chengzr01 <ziruicheng01@gmail.com>
2024-04-16 13:50:24 +0800

    modify backend address

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/agent", {
-      target: "http://82.156.245.114:5000",
-      // target:"http://127.0.0.1:8000",
+      target: "http://ec2-18-206-229-27.compute-1.amazonaws.com:8000",
       changeOrigin: true,
     })
   );
