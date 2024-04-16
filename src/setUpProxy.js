01d21b5f0e0b4a83cda9531e90f25dcbe502dd7d
chengzr01 <ziruicheng01@gmail.com>
2024-04-16 20:30:22 +0800

    update

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/agent", {
-      target: "http://ec2-18-206-229-27.compute-1.amazonaws.com:8000",
+      target: "http://18.206.229.27:8000",
       changeOrigin: true,
     })
   );
